31eaa3e0f4e89055b91982afaaae1e5b55f378d3
Jessica Hom <66929504+jessicahom@users.noreply.github.com>
2020-11-06 19:03:07 -0800

    Add greeting function

--- mixedMessages.js
+++ mixedMessages.js
@@ -2,3 +2,20 @@
 // Ask question
 // Encouragement
 
+let message = '';
+
+function addGreeting (hour) {
+    if (hour >= 5 && hour <= 12) {
+        message += 'Good morning!';
+    } else if (hour > 12 && hour <= 17) {
+        message += 'Good afternoon!';
+    } else if (hour > 17 && hour <= 23) {
+        message += 'Good evening!';
+    } else {
+        message += 'Hello!';
+    }
+}
+
+addGreeting(0);
+
+console.log(message);
